6d660f5b895744ec85c625992c8b3e646d08c28d
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:05:35 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,44 @@
+#if-elif-else ladder
+
+a=30
+
+'''if(a>3):
+    print("the value of a is greater than 3")
+elif(a>17):
+    print("the value of a is greater than 17")
+else:
+    print("the vakue of a is greaater than 3 and 17")'''
+
+#if-if-if-else
+
+'''if(a>4):
+    print("a is greater than 4")
+if(a>10):
+    print("a is greater than 10")
+else:
+    print("a is greater than 4 and 10") '''   
+
+#logical operator and or not
+
+'''age=int(input("Enter your age\n"))
+
+if (age>18) and (age < 20):
+    print("you are in your teen ages")
+if(age > 19) or (age < 30):
+    print("you are eligible")
+#if( not true):
+    print(" ")'''
+
+    #is and in
+
+'''a=12
+if(a is 12):
+    print("yes")
+else:
+    print("no")
+
+b=[12,45,67,89]
+print(12 in b)
+print(9 in b)
+if(45 in b):
+    print("yes")'''
