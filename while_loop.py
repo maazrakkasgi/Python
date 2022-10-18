59f637b13d68960b484eba45dc155f8d4a91a30a
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:07:11 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,25 @@
+'''# while loop
+
+i=0
+while i<10:
+    print("Hello World")
+    i=i+1
+
+# program to print 0 to 50 using while loop
+
+
+i=0
+while i<=50:
+    print(i)
+    i=i+1'''
+
+#program to print list elements using while loop
+
+a=["adil","maaz","sajeed","vinit","Avi"]
+i=0
+while i<len(a):
+    print(a[i])
+    i=i+1
+
+ 
+
