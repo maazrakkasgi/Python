b54922957f321df3f37b8c2c9620f29e39059990
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:06:45 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,31 @@
+'''a=["adil","maaz","sajeed","vinit","Avi"]
+for name in a:
+    print(name)'''
+
+''' #range function
+for i in range(1,11):
+    print(i)'''
+
+'''#for with else
+for i in range(1,11):
+    print(i)
+else:
+    print("I am Else")'''
+
+'''for i in range(1,11):
+    print(i)
+    if i==5:
+     break
+else:
+    print("I am Else")'''
+
+'''for i in range(1,11):
+    if i%2==0:
+        continue
+    print(i)'''
+
+'''i=4
+if i>0:
+    pass #it is null statement we can use it as do nothing it can be used with if,for,while and also functions
+print("Hello")'''
+
