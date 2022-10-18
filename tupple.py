2f190147911641e0639c73e81e53a7243ed6da61
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:03:12 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,8 @@
+t=()  #empty tupple
+t1=(1,) #tupple with one element
+t2=(1,3,4,7,9,0,2,5,3,45,786)
+#tupple are immutable they cant be changed for ex t2[2]=567 is not possible
+print(t2)
+
+print(t2.count(3))
+print(t2.index(3))
