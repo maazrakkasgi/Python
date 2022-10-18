17ab1d4744978bd9721ae89e0ba797aced1f0cff
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:07:33 +0530

    Add files via upload

---
+++ ch3ps.py
@@ -0,0 +1,34 @@
+'''   #1
+name=input("Enter your name\n")
+print("Good after noon" +name)'''
+
+#2
+
+'''letter= Dear Name congratulations you are selected
+            Date
+name=input("Enter the name")
+date=input("Enter the date")
+letter =letter.replace("Name",name)
+letter=letter.replace("Date",date)             
+print(letter)'''
+
+#3
+
+'''string="you are so cute and  handsome"
+st=string.find("  ")
+print(st)'''
+
+#4
+'''
+string="you are so cute  and  handsome"
+st=string.find("  ")
+print(st)
+string=string.replace("  "," ")
+print(string)'''
+
+#5
+'''
+letter="dear ,maaz this course is nice! you learning it properly i am very glad"
+print(letter)
+fl=("dear maaz\n this course is nice!\nyou learning it properly \t iam very glad")
+print(fl)'''
