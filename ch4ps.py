dfcd6410ca496f666f8a5afe354f7595b62f76e2
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2022-10-18 22:03:44 +0530

    Add files via upload

---
+++ ch4ps.py
@@ -0,0 +1,44 @@
+#1 write a program to store 7 fruits in a list in a list entered by user
+'''f1=input("Enter the fruit no 1")
+f2=input("Enter the fruit no 2")
+f3=input("Enter the fruit no 3")
+f4=input("Enter the fruit no 4")
+f5=input("Enter the fruit no 5")
+f6=input("Enter the fruit no 6")
+f7=input("Enter the fruit no 7")
+
+mylist=[f1,f2,f3,f4,f5,f6,f7]
+print(mylist)'''
+
+#2 write a program to accept marks of 6 students and display them in sorted manner
+
+'''f1=int(input("Enter the marks of student 1"))
+f2=int(input("Enter the marks of student 2"))
+f3=int(input("Enter the marks of student 3"))
+f4=int(input("Enter the marks of student 4"))
+f5=int(input("Enter the marks of student 5"))
+f6=int(input("Enter the marks of student 6"))
+
+
+marks=[f1,f2,f3,f4,f5,f6]
+marks.sort()
+print(marks)'''
+
+#3 check that a tupple cannot be changed in python
+
+'''a=(1,2,3,4,5,6,7)
+a[1]=45
+print(a)'''
+
+#4 write a program to sum a list with 4 no
+
+'''a=[1,4,5,8]
+sum1=a[0]+a[1]+a[2]+a[3]
+print(sum1)
+print(sum(a))'''
+
+#5 Write a no of tupple in following tupple
+'''a=(7,0,8,0,0,9)
+print(a.count(0))'''
+
+
